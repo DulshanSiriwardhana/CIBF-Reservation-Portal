bf623a9d3780f950a8448d55ae2e482e6a749b5b
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-09 02:32:37 +0530

    rabitmq init

---
+++ notification-service/src/main/java/com/example/notification_service/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.example.notificationservice.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String RESERVATION_EMAIL_QUEUE = "reservation.email.queue";
+
+    @Bean
+    public Queue reservationEmailQueue() {
+        return new Queue(RESERVATION_EMAIL_QUEUE, true);
+    }
+}
