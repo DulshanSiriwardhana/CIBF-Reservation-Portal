ed743ff1a28f8c618a5517a81f182adedd1eded5
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-12 16:58:01 +0530

    fonts, css, folders init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
-
   return (
-    <div></div>
-  )
+    <BrowserRouter>
+      <div className="font-family-notoserif">
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/AuthPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthPage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Auth Page</h1>
+    </div>
+  );
+};
+
+export default AuthPage;
