d4fe0c8706a2a7828a6abbe620c9eb50b9ec9b5e
VishSeran <vishwa.sovis@gmail.com>
2025-11-17 12:31:30 +0530

    email service running on nginx fixed

--- email-service/src/main/java/lk/bookfair/emailservice/controller/EmailController.java
+++ email-service/src/main/java/lk/bookfair/emailservice/controller/EmailController.java
@@ -32,11 +32,17 @@
 
 package lk.bookfair.emailservice.controller;
 
-import lk.bookfair.emailservice.model.EmailRequest;
-import lk.bookfair.emailservice.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.bookfair.emailservice.model.EmailRequest;
+import lk.bookfair.emailservice.service.EmailSenderService;
 
 @RestController
 @RequestMapping("/api/email")
@@ -46,7 +52,7 @@ public class EmailController {
     @Autowired
     private EmailSenderService emailSenderService;
 
-    @GetMapping("/")
+    @GetMapping({"","/"})
     public String home() {
         return "Email Service is running!";
     }
